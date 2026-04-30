66c86e3c328cc74076d99431ba0aa4d3305d3f8a
Lujan448 <LUJAN.UHALDE@correo.ucu.edu.uy>
2026-04-30 18:04:16 -0300

    arregle el program

---
+++ src/Library/Helmet.cs
@@ -0,0 +1,34 @@
+//Es la clase experta de la información que corresponde a el casco.
+//Se aplica SRP separándola de Elf, ya que si la lógica del escudo viviera dentro de Elf,
+//esa clase tendría más de una razón de cambio.
+//De esta forma, cualquier modificación relacionada al escudo se realiza únicamente acá.
+using Ucu.Poo.RolePlayGame;
+
+namespace Elfs
+{
+public class Helmet : IItems
+    {
+        //Se inicializan aquellas responsabilidades de conocer de la clase Helmet
+        //Valor de ataque y de defensa.
+        private int attackValue;
+        public int AttackValue 
+        {
+            get {return attackValue; }
+            set {attackValue = value;} 
+        }
+
+        private int defenseValue;
+        public int DefenseValue 
+        {
+            get {return defenseValue; } set {defenseValue = value; } 
+        }
+
+        //Ceamos al constructor 
+        //En este caso como es parte de la armadura en si, su valor de ataque va a ser 0.
+        public Helmet(int DefenseValue)
+        {
+            this.AttackValue = 0;
+            this.DefenseValue = DefenseValue;
+        }
+    }
+}
